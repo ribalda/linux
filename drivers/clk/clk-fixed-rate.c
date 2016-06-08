3368f373e44ebc1430da42bdc2dc68df8f73f3df
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2016-06-08 12:11:21 +0200

    clk: fixed-rate: Use CLK_OF_DECLARE_DRIVER() macro.
    Adds support for fixed-rate clock providers which have not been
    enabled via of_clk_init().
    
    This is required by Device trees overlays that introduce clocks
    providers.

--- drivers/clk/clk-fixed-rate.c
+++ drivers/clk/clk-fixed-rate.c
@@ -15,6 +15,7 @@
 #include <linux/io.h>
 #include <linux/err.h>
 #include <linux/of.h>
+#include <linux/platform_device.h>
 
 /*
  * DOC: basic fixed-rate clock that cannot gate
@@ -146,18 +147,26 @@ void clk_unregister_fixed_rate(struct clk *clk)
 EXPORT_SYMBOL_GPL(clk_unregister_fixed_rate);
 
 #ifdef CONFIG_OF
+
+int of_fixed_clk_remove(struct clk *clk)
+{
+	clk_unregister_fixed_rate(clk);
+	return 0;
+}
+
 /**
  * of_fixed_clk_setup() - Setup function for simple fixed rate clock
  */
-void of_fixed_clk_setup(struct device_node *node)
+struct clk *_of_fixed_clk_setup(struct device_node *node)
 {
 	struct clk *clk;
 	const char *clk_name = node->name;
 	u32 rate;
 	u32 accuracy = 0;
+	int ret;
 
 	if (of_property_read_u32(node, "clock-frequency", &rate))
-		return;
+		return ERR_PTR(-EIO);
 
 	of_property_read_u32(node, "clock-accuracy", &accuracy);
 
@@ -165,9 +174,24 @@ void of_fixed_clk_setup(struct device_node *node)
 
 	clk = clk_register_fixed_rate_with_accuracy(NULL, clk_name, NULL,
 						    0, rate, accuracy);
-	if (!IS_ERR(clk))
-		of_clk_add_provider(node, of_clk_src_simple_get, clk);
+	if (IS_ERR(clk))
+		return clk;
+
+	ret = of_clk_add_provider(node, of_clk_src_simple_get, clk);
+	if (ret) {
+		clk_unregister(clk);
+		return ERR_PTR(ret);
+	}
+
+	return clk;
+}
+
+void of_fixed_clk_setup(struct device_node *node)
+{
+	_of_fixed_clk_setup(node);
 }
 EXPORT_SYMBOL_GPL(of_fixed_clk_setup);
-CLK_OF_DECLARE(fixed_clk, "fixed-clock", of_fixed_clk_setup);
+
+CLK_OF_DECLARE_DRIVER(fixed_clk, "fixed-clock", _of_fixed_clk_setup,
+		      of_fixed_clk_remove);
 #endif
