e4e4fd2581ff838559f5e860464c50f8a5d40e86
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2018-10-03 21:30:15 +0200

    mtd: maps: gpio-addr-flash: Add support for device-tree devices
    Allow creating gpio-addr-flash via device-tree and not just via platform
    data.
    
    Mimic what physmap_of_versatile and physmap_of_gemini does to reduce
    code duplicity.
    
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>

---
+++ drivers/mtd/maps/physmap_of_gpio.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright 2018 Qtechnology A/S
+ *
+ * Ricardo Ribalda <ricardo.ribalda@gmail.com>
+ *
+ */
+#include <linux/platform_device.h>
+#include "gpio-addr-flash.h"
+
+int of_flash_probe_gpio(struct platform_device *pdev, struct device_node *np,
+			struct map_info *map)
+{
+	struct async_state *state;
+
+	if (!of_device_is_compatible(np, "mtd,gpio-addr-flash"))
+		return 0;
+
+	state = devm_kzalloc(&pdev->dev, sizeof(*state), GFP_KERNEL);
+	if (!state)
+		return -ENOMEM;
+
+	return gpio_flash_probe_common(&pdev->dev, state, map);
+}
