ccc1a354d10f93d1d7561b676109b75301366618
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2018-10-03 21:30:15 +0200

    mtd: maps: gpio-addr-flash: Add support for device-tree devices
    Allow creating gpio-addr-flash via device-tree and not just via platform
    data.
    
    Mimic what physmap_of_versatile and physmap_of_gemini does to reduce
    code duplicity.
    
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>

---
+++ drivers/mtd/maps/gpio-addr-flash.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/of.h>
+#include <linux/mtd/map.h>
+
+/**
+ * struct async_state - keep GPIO flash state
+ *	@gpios:       Struct containing the array of GPIO descriptors
+ *	@gpio_values: cached GPIO values
+ *	@win_order:   dedicated memory size (if no GPIOs)
+ */
+struct async_state {
+	struct gpio_descs *gpios;
+	unsigned int gpio_values;
+	unsigned int win_order;
+};
+
+int gpio_flash_probe_common(struct device *dev,
+				   struct async_state *state,
+				   struct map_info *map);
+
+#ifdef CONFIG_MTD_PHYSMAP_OF_GPIO
+int of_flash_probe_gpio(struct platform_device *pdev, struct device_node *np,
+			struct map_info *map);
+#else
+static inline
+int of_flash_probe_gpio(struct platform_device *pdev, struct device_node *np,
+			struct map_info *map)
+{
+	if (of_find_property(np, "addr-gpios", NULL))
+		return -EINVAL;
+
+	return 0;
+}
+#endif
---
+++ drivers/mtd/maps/physmap_of_gpio.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright 2018 Qtechnology A/S
+ *
+ * Ricardo Ribalda <ricardo.ribalda@gmail.com>
+ *
+ */
+#include <linux/platform_device.h>
+#include "gpio-addr-flash.h"
+
+int of_flash_probe_gpio(struct platform_device *pdev, struct device_node *np,
+			struct map_info *map)
+{
+	struct async_state *state;
+
+	if (!of_find_property(np, "addr-gpios", NULL))
+		return -EINVAL;
+
+	state = devm_kzalloc(&pdev->dev, sizeof(*state), GFP_KERNEL);
+	if (!state)
+		return -ENOMEM;
+
+	return gpio_flash_probe_common(&pdev->dev, state, map);
+}
